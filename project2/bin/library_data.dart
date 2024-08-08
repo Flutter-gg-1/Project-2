2c9706e0c100ae8d5fdb1bdaa78696738f78b0a6
Amjad Elahi <amjadelahi07@gmail.com>
2024-08-08 17:04:07 +0300

    add library data sets

---
+++ project2/bin/library_data.dart
@@ -0,0 +1,44 @@
+final libraryData = {
+  "library": [
+    {
+      "id": "1",
+      "title": "The Great Gatsby",
+      "authors": ["F. Scott Fitzgerald"],
+      "categories": ["Fiction"],
+      "year": 1925,
+      "quantity": 5
+    },
+    {
+      "id": "2",
+      "title": "To Kill a Mockingbird",
+      "authors": ["Harper Lee"],
+      "categories": ["Fiction", "Classic"],
+      "year": 1960,
+      "quantity": 0
+    },
+    {
+      "id": "3",
+      "title": "1984",
+      "authors": ["George Orwell"],
+      "categories": ["Dystopian", "Science Fiction"],
+      "year": 1949,
+      "quantity": 3
+    },
+    {
+      "id": "4",
+      "title": "Pride and Prejudice",
+      "authors": ["Jane Austen"],
+      "categories": ["Romance", "Classic"],
+      "year": 1813,
+      "quantity": 4
+    },
+    {
+      "id": "5",
+      "title": "The Catcher in the Rye",
+      "authors": ["J.D. Salinger"],
+      "categories": ["Fiction"],
+      "year": 1951,
+      "quantity": 2
+    }
+  ]
+};
