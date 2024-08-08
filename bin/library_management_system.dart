11c11b91df1064bf59d02c2cb22dd9c9f7a070a5
turki-aloufi <turkialoufi44@gmail.com>
2024-08-08 17:56:00 +0300

    initial commit

---
+++ bin/library_management_system.dart
@@ -0,0 +1,5 @@
+import 'package:library_management_system/library_management_system.dart' as library_management_system;
+
+void main(List<String> arguments) {
+  print('Hello world: ${library_management_system.calculate()}!');
+}
