a25eb4a4c698492439d1db84b57438e65871d6bf
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-09 08:19:31 +0300

    Projet Worked

--- bin/library_system.dart
+++ bin/library_system.dart
@@ -1,5 +1,4 @@
-import 'package:library_system/library_system.dart' as library_system;
 
-void main(List<String> arguments) {
-  print('Hello world: ${library_system.calculate()}!');
+void main() {
+  print("Hello, World!");
 }
