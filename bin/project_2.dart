2252f61b8a0a7a6b3bd131156980487bd7da39aa
MAHA Al-ATAWI <mem880.a@gmail.com>
2024-08-10 20:38:47 +0300

    Removing unused packages at main page & var

--- bin/project_2.dart
+++ bin/project_2.dart
@@ -1,23 +1,9 @@
 import 'management.dart';
-import 'user.dart';
-import 'admin.dart';
 
 void main() {
-  // // display all data
-  // Library library = Library.fromJson(libraryBook);
-  // for (var book in library.books) {
-  //   print(book.title);
-  //   print(book.authors);
-  //   print(book.categories);
-  //   print(book.year);
-  //   print(book.quantity);
-  //   print(book.price);
-  // }
-  bool isExit = false;
   print("Welcome to the library");
   print("Happy to see you again !");
 
   Accounts accounts = Accounts();
   accounts.logIn();
-  
 }
