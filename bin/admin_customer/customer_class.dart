13845c95bd13420017dec4dc66412178259e6800
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-10 11:17:58 +0300

    UPD Customer Dashboard 0.0.0

--- bin/admin_customer/customer_class.dart
+++ bin/admin_customer/customer_class.dart
@@ -2,5 +2,28 @@ import 'dart:io';
 import '../core/all_file.dart';
 
 void customerDashboard(){
-  print('Customer');
+  print('\n_____________#### Customer Dashboard ####_____________\n');
+  print('1. Purchase a book, 2. Show receipt, 3. Exit');
+  try {
+    int customerOption = int.parse(stdin.readLineSync()!);
+    switch (customerOption) {
+      case 1:
+        purchaseBook();
+        break;
+      case 2:
+        showReceipt();
+        break;
+      case 3:
+        print('Bye.');
+        exit(0);
+      default:
+        print('Invalid input. Please try again.');
+        customerDashboard();
+        break;
+    }
+  } catch (e) {
+    print('Invalid input...${e.toString()}. Please try again.');
+    customerDashboard();
+  }
+
 }
