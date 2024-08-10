26372630489b3ee2d46edb9d39ebf2b9e973f4c4
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-10 14:38:58 +0300

    UPD Customer Dashboard 0.0.1

--- bin/admin_customer/customer_class.dart
+++ bin/admin_customer/customer_class.dart
@@ -3,7 +3,7 @@ import '../core/all_file.dart';
 
 void customerDashboard(){
   print('\n_____________#### Customer Dashboard ####_____________\n');
-  print('1. Purchase a book, 2. Show receipt, 3. Exit');
+  print('1. Purchase a book, 2. Show receipt, 3. Main Dashboard, 4. Exit');
   try {
     int customerOption = int.parse(stdin.readLineSync()!);
     switch (customerOption) {
@@ -14,6 +14,9 @@ void customerDashboard(){
         showReceipt();
         break;
       case 3:
+        mainDashboard();
+        break;
+      case 4:
         print('Bye.');
         exit(0);
       default:
