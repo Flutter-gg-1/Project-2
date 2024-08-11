04e665956c56e3afba4fed27134e7afc62b9c3dc
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-11 04:40:23 +0300

    ADD Show Receipt & Done

--- bin/features/show_a_receipt.dart
+++ bin/features/show_a_receipt.dart
@@ -1,6 +1,14 @@
 // Implement Operations : Display Receipt: Develop a function to display a receipt for the purchased books.
-
+import '../core/all_file.dart';
 void showReceipt() {
   print('\n___________________#### Receipt ####__________________\n');
-  
+  if (purchaseINFO.isEmpty) {
+    print('No book purchased yet.');
+  } else {
+    print('Book Purchased:');
+    for (var element in purchaseINFO) {
+      print(element);
+    }
+  }
+  showPrompt(); 
 }
